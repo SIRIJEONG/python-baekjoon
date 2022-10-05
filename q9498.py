ca4192daf7e1e66dfd1ca76a48fe6197fb040b76
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-10-05 20:22:30 +0900

    quest

---
+++ q9498.py
@@ -0,0 +1,12 @@
+b = int(input())
+
+if 100 >= b >= 90:
+    print('A')
+elif 89 >= b >= 80:
+    print('B')
+elif 79 >= b >= 70:
+    print('C')
+elif 69 >= b >= 60:
+    print('D')
+else:
+    print('F')
