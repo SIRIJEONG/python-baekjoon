2242e328f447a75f4d7871f8f343df7fa8c5876f
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-10-05 20:08:34 +0900

    quest

---
+++ q1330.py
@@ -0,0 +1,13 @@
+a,b = input().split()
+
+x = int(a)
+y = int(b)
+
+
+
+if x < y:
+    print("<")
+elif x > y:
+    print(">")
+else:
+    print("==")
---
+++ q2588.py
@@ -0,0 +1,14 @@
+# 세자리수 곱하기 세자리수 출력
+
+a = int(input()) #숫자로 변환
+b = input() #문자열 그대로
+
+axb2 = a*int(b[2])
+axb1 = a*int(b[1])
+axb0 = a*int(b[0])
+axb = a*int(b)
+
+print(axb2)
+print(axb1)
+print(axb0)
+print(axb)
