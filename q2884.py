44f132393dbaa1a5f2f0acd243edc7180ed6224e
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-10-10 23:16:28 +0900

    quest

--- q2884.py
+++ q2884.py
@@ -3,3 +3,12 @@ a,b = input().split()
 h = int(a)
 m = int(b)
 
+if 1 <= h <=23 and 0 <= m < 45:
+    print((h-1) , 60-(45-m))
+elif h == 0 and 0 <= m < 45:
+    print(23 , 60-(45-m))
+elif h == 0 and 45 <= m <60:
+    print(0, m-45)
+else:
+    print(h,m-45)
+
