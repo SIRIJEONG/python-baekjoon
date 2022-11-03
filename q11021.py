7ea4cff4eb7429db8a61e8677ee5ad1040788a33
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-11-04 00:06:55 +0900

    quest

---
+++ q11021.py
@@ -0,0 +1,15 @@
+# a+b-7
+
+#t = int(input())
+
+
+for i in range(1, t+1):
+    a,b = map(int , input().split())
+    print(f'Case #{i}: , {a+b}')
+# f-string을 이용하여 문자열안에 {}를 입력하고 괄호안에 변수나 변수를 연산값으로 입력할수 있다.
+
+t = int(input())
+
+for i in range(1, t+1):
+    a,b = map(int , input().split())
+    print("Case #" + str(i)+':',a+b)
