91f75b012e580e537b30465d7a07707cea7d2679
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-11-11 18:56:18 +0900

    quest

---
+++ q10818.py
@@ -0,0 +1,15 @@
+# 최소 최대
+
+#n = int(input())
+
+#n_list = list(map(int, input().split()))
+#n_list.sort()
+#print(n_list[0] , n_list.pop())
+# 내가푼방법
+
+n = int(input())
+
+n_list = list(map(int, input().split()))
+
+print(min(n_list),max(n_list))
+# min max를 쓰면 최대 최솟값을 알수있다.
