0842b2bdaca04ac2edd47d01bfdaadccf4436bb9
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-11-17 00:03:27 +0900

    quest

--- q1546.py
+++ q1546.py
@@ -1,8 +1,11 @@
 # 평균
 
 n= int(input())
+n_list = list(map(int, input().split()))
+m = max(n_list)
+sum = 0
 
 for i in range(n):
-    n_list = list(map(int, input().split()))
-
-    print(max(n_list))
+    n_list[i]=n_list[i]/m*100
+    sum += n_list[i]
+print(sum/n)
