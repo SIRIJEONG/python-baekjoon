1c63acd353075ccce8ae8aa1397fe6200a593374
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-12-05 21:27:47 +0900

    quest

--- q1157.py
+++ q1157.py
@@ -1,15 +1,15 @@
 # 단어공부
 
 n = input().upper()
-n.list = list(set(n))
+n_list = list(set(n))
 
 a = []
 
-for i in n.list:
-    b = n.count()
+for i in n_list:
+    b = n.count
     a.append(b(i))
 
 if a.count(max(a)) > 1:
     print("?")
 else:
-    print(n.list[(a.index(max(a)))])
+    print(n_list[(a.index(max(a)))])
