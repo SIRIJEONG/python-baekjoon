9fa94ceea77fa7c5cdf023a3b2987990313b0fd0
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-12-12 20:33:59 +0900

    quest

--- q1712.py
+++ q1712.py
@@ -1,9 +1,11 @@
 # 손익 분기점
 
-a,b,c = int(input().split())
-n = 0
+a,b,c = map(int,input().split())
 #a는 매년 고정비용 b는 노트북 한대의 재료비 인건비 c는 노트북 가격
 # 총수입이 총비용보다 많아지게 되는 손익분기점을 구하는 프로그램
 
-while a + (b*n) >
 
+if b >= c:
+    print(-1)
+else:
+    print(int(a/(c-b)+1))
