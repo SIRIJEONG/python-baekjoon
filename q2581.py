0e1abc55598dbb612df428e7f7af8b127b0f25ce
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2023-01-03 19:45:33 +0900

    quest

--- q2581.py
+++ q2581.py
@@ -1,22 +1,44 @@
 # 소수
 
+# m = int(input())
+# n = int(input())
+# n_list = []
+# for i in range(m,n+1):
+#     if i == 1:
+#         pass
+#     elif i == 2:
+#         n_list.append(i)
+#     else:
+#         for j in range(2,j):
+#             if i%j == 0:
+#                 break
+#             elif j == i-1:
+#                 n_list.append(i)
+# if len(n_list)==0:
+#     print(-1)
+# else:
+#     print(sum(n_list))
+#     print(min(n_list))
+
 m = int(input())
 n = int(input())
 n_list = []
+
 for i in range(m,n+1):
-    if i == 1:
-        pass
-    elif i == 2:
-        n_list.append(i)
-    else:
-        for j in range(2,j):
-            if i%j == 0:
+    error = 0
+    if i > 1:
+        for j in range(2,i):
+            if i % j == 0:
+                error += 1
                 break
-            elif j == i-1:
-                n_list.append(i)
-if len(n_list)==0:
-    print(-1)
-else:
+        if error == 0:
+            n_list.append(i)
+
+if len(n_list) > 0:
     print(sum(n_list))
     print(min(n_list))
+else:
+    print(-1)
+
+
 
