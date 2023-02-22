5038eb8514b933b9189264f04db403e7900089b0
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2023-02-22 23:01:48 +0900

    quest

--- q2231.py
+++ q2231.py
@@ -2,3 +2,16 @@
 
 n = int(input())
 
+a = 0
+
+for i in range(1,n+1):
+    b = list(map(int,str(i)))
+    a = i + sum(b)
+    if a == n:
+        print(i)
+        break
+
+    if i == n:
+        print(0)
+
+
